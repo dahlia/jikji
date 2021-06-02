25c06774d09810db9ffe4d6cd764a1ea271c1b63
Hong Minhee <hong.minhee@gmail.com>
2021-06-03 02:36:49 +0900

    Media types

---
+++ media_type.ts
@@ -0,0 +1,195 @@
+/**
+ * Represents IANA media types, formerly known as MIME types.
+ *
+ * It guarantees all instances are interned and only single instance is made
+ * for each IANA media type, which means, `===` operator can be used for testing
+ * structural equality of two {@link MediaType} operands.
+ */
+export class MediaType {
+  /** A major media type, e.g., `text`, `image`, `application`. */
+  readonly type: string;
+  /**
+   * A tree of media subtype, e.g., `plain`, `jpeg`, `vnd.ms-excel`.
+   * Each node of tree is represented as an element of array,
+   * e.g., `["vnd", "ms-excel"]`.
+   */
+  readonly subtype: [string, ...string[]];
+  /** Zero or more subtype suffixes, e.g., `xml` from `image/svg+xml`. */
+  readonly suffixes: string[];
+  /**
+   * Zero or more key-value pairs,
+   * e.g., `charset` & `utf-8` from `text/html; charset=utf-8`.
+   */
+  readonly parameters: Record<string, string>;
+
+  private constructor(
+    type: string,
+    subtype: [string, ...string[]],
+    suffixes?: string[],
+    parameters?: Record<string, string>,
+  ) {
+    const fields: Record<string, string | string[] | null | undefined> = {
+      type,
+      "node of subtype": subtype,
+      suffix: suffixes,
+    };
+    for (const name in fields) {
+      const f = fields[name];
+      const vs = f == null ? [] : typeof f == "string" ? [f] : f;
+      for (const v of vs) {
+        if (v.match(/[/.+;\s]/)) {
+          throw new MediaTypeError(
+            `A ${name} cannot contains any whitespace, slashes, periods, ` +
+              `plus, or semicolon: ${JSON.stringify(v)}.`,
+          );
+        } else if (v.length < 1) {
+          throw new MediaTypeError(`A ${name} cannot be empty.`);
+        }
+      }
+    }
+    this.type = type.toLowerCase();
+    const normalizedSubtype = subtype.map((s) => s.toLowerCase());
+    this.subtype = [normalizedSubtype[0], ...normalizedSubtype.slice(1)];
+    Object.freeze(this.subtype);
+    this.suffixes = suffixes || [];
+    Object.freeze(this.suffixes);
+    this.parameters = {};
+    for (const attr in parameters) {
+      if (attr.length < 1) {
+        throw new MediaTypeError("Parameter names cannot be empty.");
+      }
+      const illegalChars = attr.match(/[^-!#$%&'*+.0-9<>A-Z^_`a-z{|}~]+/);
+      if (illegalChars != null) {
+        throw new MediaTypeError(
+          `There are some disallowed characters ${
+            JSON.stringify(illegalChars[0])
+          } in a parameter name: ${JSON.stringify(attr)}.`,
+        );
+      }
+      const a = attr.toLowerCase();
+      if (this.parameters[a] == null) {
+        this.parameters[a] = parameters[attr];
+      } else {
+        throw new MediaTypeError(
+          `There are duplicate parameter names: ${
+            JSON.stringify(a)
+          }.  Note that parameter names are case-insensitive.`,
+        );
+      }
+    }
+    Object.freeze(this.parameters);
+    Object.freeze(this);
+  }
+
+  private static readonly interns: Record<string, MediaType> = {};
+
+  /**
+   * Gets a {@link MediaType} instance.
+   * @param type A major media type, e.g., `text`, `image`, `application`.
+   * @param subtype A tree of media subtype, e.g., `plain`, `jpeg`,
+   *                `vnd.ms-excel`.  Each node of tree is represented as
+   *                an element of array, e.g., `["vnd", "ms-excel"]`.
+   * @param parameters Zero or more key-value pairs, e.g.,
+   *                   `charset` & `utf-8` from `text/html; charset=utf-8`.
+   * @returns A corresponding {@link MediaType} instance.
+   * @throws {MediaTypeError} Thrown when any parameter is invalid.
+   */
+  static get(
+    type: string,
+    subtype: [string, ...string[]] | string,
+    suffixes?: string[],
+    parameters?: Record<string, string>,
+  ): MediaType {
+    if (typeof subtype == "string") {
+      const split = subtype.split(".");
+      if (split.length < 1 || split[0].trim() === "") {
+        throw new MediaTypeError("A subtype cannot be empty.");
+      }
+      subtype = split as [string, ...string[]];
+    }
+    const mediaType = new this(type, subtype, suffixes, parameters);
+    const key = mediaType.toString();
+    const interned = this.interns[key];
+    return interned || (this.interns[key] = mediaType);
+  }
+
+  private static readonly PATTERN: RegExp =
+    /^([^/.+;\s]+)\/([^/.+;\s]+)((?:\.[^/.+;\s]+)*)((\+[^/.+;\s]+)*)(\s*;.*)?$/;
+
+  private static readonly PARAMETER_PATTERN: RegExp =
+    /;\s*([-!#$%&'*+.0-9<>A-Z^_`a-z{|}~]+)\s*=\s*("([^"\\\s]| |\\.)*"|[^()<>@,;:\\"\/\[\]?=\s]+)(?=;|$)/g;
+
+  /**
+   * Parses an IANA media type string.
+   * @param mediaType An IANA media type.
+   * @returns A corresponding {@link MediaType} instance.
+   * @throws {MediaTypeError} Thrown when the given argument is not a valid
+   *         IANA media type.
+   */
+  static fromString(mediaType: string): MediaType {
+    const match = this.PATTERN.exec(mediaType);
+    if (match == null) {
+      throw new MediaTypeError(
+        `Not a correct IANA media type string: ${JSON.stringify(mediaType)}.`,
+      );
+    }
+    const subtype: [string, ...string[]] = [
+      match[2],
+      ...match[3].split(".").slice(1),
+    ];
+    const suffixes = match[5] == null ? [] : match[5].split("+").slice(1);
+    const paramMatches = match[6] == null
+      ? []
+      : [...match[6].matchAll(this.PARAMETER_PATTERN)];
+    if (match[6] != null && paramMatches.length < 1 && match[6].trim() !== "") {
+      throw new MediaTypeError(
+        `Incorrect parameters: ${JSON.stringify(match[6])}.`,
+      );
+    }
+    const params: Record<string, string> = {};
+    for (const paramMatch of paramMatches) {
+      const paramName = paramMatch[1];
+      const paramValue = paramMatch[2].startsWith('"')
+        ? this.unquoteParameterValue(paramMatch[2])
+        : paramMatch[2];
+      params[paramName] = paramValue;
+    }
+    return this.get(match[1], subtype, suffixes, params);
+  }
+
+  private static unquoteParameterValue(quoted: string): string {
+    // Assuming quoted starts and ends with a pair of `"`s.
+    const bare = quoted.substr(1, quoted.length - 2);
+    return bare.replace(/\\(.)/g, "$1");
+  }
+
+  /**
+   * Turns a media type object into an IANA media type string.
+   * @returns A string formatted as IANA media type.
+   */
+  toString(): string {
+    const suffixes = this.suffixes.map((s) => `+${s}`).join("");
+    const params = Object.entries(this.parameters)
+      .sort((a, b) => a[0] == b[0] ? 0 : a[0] < b[0] ? -1 : 1)
+      .map((kv) => `; ${kv[0]}=${MediaType.escapeParameterValue(kv[1])}`)
+      .join("");
+    return `${this.type}/${this.subtype.join(".")}${suffixes}${params}`;
+  }
+
+  private static readonly BARE_PARAMETER_VALUE_PATTERN: RegExp =
+    /^[^()<>@,;:\\"\/\[\]?=]+$/;
+
+  private static escapeParameterValue(value: string): string {
+    if (value.match(this.BARE_PARAMETER_VALUE_PATTERN)) {
+      return value;
+    }
+
+    return `"${value.replace(/([\s"\\])/g, "\\$1")}"`;
+  }
+}
+
+/** Thrown when an invalid IANA media type is tried to be made. */
+export class MediaTypeError extends Error {
+}
+
+export default MediaType;
---
+++ media_type_test.ts
@@ -0,0 +1,189 @@
+import {
+  assertEquals,
+  assertStrictEquals,
+  assertThrows,
+} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+import { MediaType, MediaTypeError } from "./media_type.ts";
+
+Deno.test("MediaType.get()", () => {
+  // text/plain
+  const txt = MediaType.get("Text", "Plain");
+  assertStrictEquals(txt.type, "text");
+  assertEquals(txt.subtype, ["plain"]);
+  assertEquals(txt.suffixes, []);
+  assertEquals(txt.parameters, {});
+  // application/vnd.oasis.opendocument.text:
+  const odt = MediaType.get("application", [
+    "Vnd",
+    "Oasis",
+    "OpenDocument",
+    "Text",
+  ]);
+  assertStrictEquals(odt.type, "application");
+  assertEquals(odt.subtype, ["vnd", "oasis", "opendocument", "text"]);
+  assertEquals(odt.suffixes, []);
+  assertEquals(odt.parameters, {});
+  // application/xhtml+xml; charset=utf-8:
+  const xhtml = MediaType.get("Application", ["xhtml"], ["xml"], {
+    CharSet: "utf-8",
+  });
+  assertStrictEquals(xhtml.type, "application");
+  assertEquals(xhtml.subtype, ["xhtml"]);
+  assertEquals(xhtml.suffixes, ["xml"]);
+  assertEquals(xhtml.parameters, { charset: "utf-8" });
+  // Interning:
+  assertStrictEquals(
+    odt,
+    MediaType.get("application", [
+      "Vnd",
+      "Oasis",
+      "OpenDocument",
+      "Text",
+    ]),
+  );
+  assertStrictEquals(
+    xhtml,
+    MediaType.get("Application", ["xhtml"], ["xml"], {
+      CharSet: "utf-8",
+    }),
+  );
+  // Invalid media types:
+  assertThrows(
+    () => MediaType.get("", ["plain"]),
+    MediaTypeError,
+    "type cannot be empty",
+  );
+  assertThrows(
+    () => MediaType.get("text ", ["plain"]),
+    MediaTypeError,
+    "type cannot contains any whitespace",
+  );
+  assertThrows(
+    () => MediaType.get("text", [""]),
+    MediaTypeError,
+    "subtype cannot be empty",
+  );
+  assertThrows(
+    () => MediaType.get("application", ["vnd", "oasis", "opendocument", ""]),
+    MediaTypeError,
+    "subtype cannot be empty",
+  );
+  assertThrows(
+    () => MediaType.get("text", ["plain "]),
+    MediaTypeError,
+    "subtype cannot contains any whitespace",
+  );
+  assertThrows(
+    () => MediaType.get("application", ["vnd", "ms-excel "]),
+    MediaTypeError,
+    "subtype cannot contains any whitespace",
+  );
+  assertThrows(
+    () => MediaType.get("application", ["xhtml"], [""]),
+    MediaTypeError,
+    "suffix cannot be empty",
+  );
+  assertThrows(
+    () => MediaType.get("application", ["xhtml"], ["xml "]),
+    MediaTypeError,
+    "suffix cannot contains any whitespace",
+  );
+  assertThrows(
+    () => MediaType.get("text", ["plain"], [], { "": "utf-8" }),
+    MediaTypeError,
+    "Parameter names cannot be empty",
+  );
+  assertThrows(
+    () => MediaType.get("text", ["plain"], [], { "char set": "utf-8" }),
+    MediaTypeError,
+    "disallowed characters",
+  );
+  assertThrows(
+    () =>
+      MediaType.get("text", ["plain"], [], {
+        charset: "utf-8",
+        CharSet: "ascii",
+      }),
+    MediaTypeError,
+    "duplicate parameter",
+  );
+});
+
+Deno.test("MediaType.fromString()", () => {
+  const odt = MediaType.fromString("application/vnd.oasis.opendocument.text");
+  assertStrictEquals(odt.type, "application");
+  assertEquals(odt.subtype, ["vnd", "oasis", "opendocument", "text"]);
+  assertEquals(odt.suffixes, []);
+  assertEquals(odt.parameters, {});
+  const xhtml = MediaType.fromString("application/xhtml+xml; charset=utf-8");
+  assertStrictEquals(xhtml.type, "application");
+  assertEquals(xhtml.subtype, ["xhtml"]);
+  assertEquals(xhtml.suffixes, ["xml"]);
+  assertEquals(xhtml.parameters, { charset: "utf-8" });
+  // Interning:
+  assertStrictEquals(
+    odt,
+    MediaType.get("application", [
+      "Vnd",
+      "Oasis",
+      "OpenDocument",
+      "Text",
+    ]),
+  );
+  assertStrictEquals(
+    xhtml,
+    MediaType.get("Application", ["xhtml"], ["xml"], {
+      CharSet: "utf-8",
+    }),
+  );
+  // Invalid media types:
+  assertThrows(() => MediaType.fromString("/plain"), MediaTypeError);
+  assertThrows(() => MediaType.fromString("text /plain"), MediaTypeError);
+  assertThrows(() => MediaType.fromString("text/"), MediaTypeError);
+  assertThrows(
+    () => MediaType.fromString("application/vnd.oasis.opendocument."),
+    MediaTypeError,
+  );
+  assertThrows(() => MediaType.fromString("text/plain "), MediaTypeError);
+  assertThrows(
+    () => MediaType.fromString("application/vnd.ms-excel "),
+    MediaTypeError,
+  );
+  assertThrows(
+    () => MediaType.fromString("application/xhtml+"),
+    MediaTypeError,
+  );
+  assertThrows(
+    () => MediaType.fromString("application/xhtml+xml "),
+    MediaTypeError,
+  );
+  assertThrows(
+    () => MediaType.fromString("text/plain; =utf-8"),
+    MediaTypeError,
+    "Incorrect parameters",
+  );
+  assertThrows(
+    () => MediaType.fromString("text/plain; char set=utf-8"),
+    MediaTypeError,
+    "Incorrect parameters",
+  );
+  assertThrows(
+    () => MediaType.fromString("text/plain; CharSet=utf-8; charset=ascii"),
+    MediaTypeError,
+    "duplicate parameter",
+  );
+});
+
+Deno.test("MediaType.toString()", () => {
+  const odt = MediaType.get("application", [
+    "Vnd",
+    "Oasis",
+    "OpenDocument",
+    "Text",
+  ]);
+  assertStrictEquals(odt.toString(), "application/vnd.oasis.opendocument.text");
+  const xhtml = MediaType.get("Application", ["xhtml"], ["xml"], {
+    CharSet: "utf-8",
+  });
+  assertStrictEquals(xhtml.toString(), "application/xhtml+xml; charset=utf-8");
+});

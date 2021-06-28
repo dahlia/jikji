6f5e5543dee91903fad8a8d1cdaa5eb4b0f6931b
Hong Minhee <hong.minhee@gmail.com>
2021-06-29 01:27:21 +0900

    Front matter & Markdown

---
+++ markdown.ts
@@ -0,0 +1,89 @@
+import MarkdownIt from "https://esm.sh/markdown-it@12.0.6";
+import abbr from "https://esm.sh/markdown-it-abbr@1.0.4";
+import attrs from "https://esm.sh/markdown-it-attrs@4.0.0";
+import deflist from "https://esm.sh/markdown-it-deflist@2.1.0";
+import footnote from "https://esm.sh/markdown-it-footnote@3.0.3";
+import title from "https://esm.sh/markdown-it-title@3.0.0";
+import * as yaml from "https://deno.land/std@0.99.0/encoding/yaml.ts";
+import { Content, ContentTransformer, MediaType } from "./pipeline.ts";
+
+/**
+ * Parses front matter in the given {@link Content} (if exists) and turn it
+ * into metadata.  Overlapped metadata keys are overwritten.
+ * Parsed front matter is removed from body.  If the body is a
+ * {@link Uint8Array}, it is decoded into a string.
+ *
+ * Designed to work with {@link Pipeline#transform} or
+ * {@link Pipeline#diversify} methods.
+ * @param content The content to parse its front matter.
+ * @returns The distinct content with front matter turned to metadata.
+ */
+export function frontMatter(content: Content) {
+  const frontMatterPattern = /^---\n((.+?\n)*?)---\n/;
+  const charset = content.type.parameters.charset;
+  return new Content(
+    async () => {
+      const metadata = await content.getMetadata();
+      let src = await content.getBody();
+      if (src instanceof Uint8Array) {
+        const decoder = new TextDecoder(charset || "utf-8");
+        src = decoder.decode(src);
+      }
+      const match = frontMatterPattern.exec(src);
+      if (match != null && match[1] != null) {
+        const frontMatter = yaml.parse(match[1]);
+        const newMetadata = typeof frontMatter == "object"
+          ? { ...metadata, ...frontMatter }
+          : { ...metadata, frontMatter };
+        return [src.slice(match[0].length), newMetadata];
+      }
+      return [await content.getBody(), metadata];
+    },
+    MediaType.get("text", "markdown", [], charset == null ? {} : { charset }),
+    content.language,
+    content.lastModified,
+  );
+}
+
+/**
+ * Creates a content transformer which compiles a Markdown into an HTML/XHTML.
+ * The used `env`ironment sandbox is merged to content metadata.  If some
+ * metadata keys are overlapped, the existing keys are overwritten.  If the body
+ * is a {@link Uint8Array}, it is decoded into a string.
+ *
+ * Designed to work with {@link Pipeline#transform} or
+ * {@link Pipeline#diversify} methods.
+ * @param markdownIt An optional instance of {@link MarkdownIt}.  If omitted,
+ *                   a new instance with default settings is used.
+ * @param env An optional environment sandbox.
+ * @returns A function to compiles a Markdown into an HTML/XHTML.
+ */
+export function markdown(
+  markdownIt?: typeof MarkdownIt,
+  env?: Record<string, unknown>,
+): ContentTransformer {
+  markdownIt ??= new MarkdownIt();
+  const outType = markdownIt.options.xhtmlOut
+    ? MediaType.fromString("application/xhtml+xml")
+    : MediaType.fromString("text/html");
+  return (content) => {
+    const charset = content.type.parameters.charset;
+    return new Content(
+      async () => {
+        const metadata = await content.getMetadata();
+        let src = await content.getBody();
+        if (src instanceof Uint8Array) {
+          const decoder = new TextDecoder(charset || "utf-8");
+          src = decoder.decode(src);
+        }
+        const newEnv = { ...env };
+        return [markdownIt.render(src, newEnv), { ...metadata, ...newEnv }];
+      },
+      charset == null ? outType : outType.withParameter("charset", charset),
+      content.language,
+      content.lastModified,
+    );
+  };
+}
+
+export { abbr, attrs, deflist, footnote, MarkdownIt, title };
---
+++ markdown_test.ts
@@ -0,0 +1,94 @@
+import { assertEquals$ } from "./asserts.ts";
+import { abbr, frontMatter, markdown, MarkdownIt, title } from "./markdown.ts";
+import { Content } from "./content.ts";
+
+Deno.test("frontMatter()", async () => {
+  const bodyTypes: (((_: string) => string) | ((_: string) => Uint8Array))[] = [
+    String,
+    (s) => new TextEncoder().encode(s),
+  ];
+  for (const bodyType of bodyTypes) {
+    const nofm = new Content(
+      bodyType("No front matter."),
+      "text/markdown",
+      "en",
+      null,
+      {
+        foo: 1,
+        bar: 2,
+      },
+    );
+    await assertEquals$(frontMatter(nofm), nofm);
+
+    const fm = new Content(
+      bodyType(
+        "---\ntitle: Front matter\nattr: true\n---\nIt has front matter.",
+      ),
+      "text/markdown",
+      "en",
+      null,
+      { foo: 1, bar: 2 },
+    );
+    await assertEquals$(
+      frontMatter(fm),
+      fm.replace({
+        body: "It has front matter.",
+        metadata: { foo: 1, bar: 2, title: "Front matter", attr: true },
+      }),
+    );
+  }
+});
+
+Deno.test("markdown()", async () => {
+  const md = new Content(
+    "# Heading 1\n\nFoo BAR baz.\n\n-----",
+    "text/markdown",
+    "en",
+    null,
+    { foo: 1, bar: 2 },
+  );
+
+  const def = markdown();
+  await assertEquals$(
+    def(md),
+    md.replace({
+      body: "<h1>Heading 1</h1>\n<p>Foo BAR baz.</p>\n<hr>\n",
+      type: "text/html",
+    }),
+  );
+
+  const xhtml = markdown(new MarkdownIt({ xhtmlOut: true }));
+  await assertEquals$(
+    xhtml(md),
+    md.replace({
+      body: "<h1>Heading 1</h1>\n<p>Foo BAR baz.</p>\n<hr />\n",
+      type: "application/xhtml+xml",
+    }),
+  );
+
+  const withAbbr = markdown(new MarkdownIt().use(abbr), {
+    abbreviations: { ":BAR": "Bay Area Reporter" },
+  });
+  await assertEquals$(
+    withAbbr(md),
+    md.replace({
+      body: '<h1>Heading 1</h1>\n<p>Foo <abbr title="Bay Area Reporter">' +
+        "BAR</abbr> baz.</p>\n<hr>\n",
+      type: "text/html",
+      metadata: {
+        ...await md.getMetadata(),
+        abbreviations: { ":BAR": "Bay Area Reporter" },
+      },
+    }),
+  );
+
+  const withTitle = markdown(new MarkdownIt().use(title));
+  await assertEquals$(
+    withTitle(md),
+    md.replace({
+      body: "<h1>Heading 1</h1>\n<p>Foo BAR baz.</p>\n<hr>\n",
+      type: "text/html",
+      metadata: { ...await md.getMetadata(), title: "Heading 1" },
+    }),
+  );
+});

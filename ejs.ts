7d5e5ef8f8950411ae266ae8db23c0a7f161ca6c
Hong Minhee <hong.minhee@gmail.com>
2021-07-18 18:39:39 +0900

    renderListTemplate() function

--- ejs.ts
+++ ejs.ts
@@ -1,7 +1,8 @@
 import { dirname, resolve } from "https://deno.land/std@0.101.0/path/mod.ts";
 import { compile } from "https://deno.land/x/dejs@0.9.3/mod.ts";
-import { Content, MediaType } from "./content.ts";
-import { ContentTransformer } from "./pipeline.ts";
+import { Content, LanguageTag, MediaType } from "./content.ts";
+import * as metadata from "./metadata.ts";
+import { ContentTransformer, Resource } from "./pipeline.ts";
 
 /**
  * Gives a {@link ContentTransformer} that compiles and renders EJS templates.
@@ -38,12 +39,7 @@ export function renderTemplate(
       type: type,
       body: async () => {
         if (tpl == null) {
-          const f = await Deno.open(filePath, { read: true });
-          try {
-            tpl = await compile(f);
-          } finally {
-            f.close();
-          }
+          tpl = await openTemplate(filePath);
         }
         return await tpl({
           ...await content.getMetadata(),
@@ -59,4 +55,79 @@ export function renderTemplate(
         : content.lastModified,
     });
   };
+}
+
+/**
+ * Renders a `list` of {@link Resource}s through an EJS template file and
+ * returns a {@link Content} with the rendered content.
+ *
+ * In the EJS template, the following variables are available:
+ *
+ * - `list`: the array of {@link Resource}s
+ * - `__file__`: the absolute path to the `templateFile`
+ * - `__dir__`: the absolute path to the directory containing `templateFile`
+ * - `query*`: the query functions defined in the *metadata.ts* module
+ * - `sortResources`: a function that sorts the resources
+ *
+ * Other keys from `context` are also available as variables in the template,
+ * and these keys can override the above variables.
+ * @param templateFile The path to the template file.
+ * @param list The list of {@link Resource}s to render.
+ * @param context The contextual variables to be passed to the template.
+ * @param type The media type.
+ * @param language The language tag.
+ * @returns A {@link Content} with the rendered content.
+ */
+export async function renderListTemplate(
+  templateFile: string,
+  list: AsyncIterable<Resource> | Iterable<Resource>,
+  context?: Record<string, unknown>,
+  type: MediaType | string = "text/html",
+  language: LanguageTag | string | null = null,
+): Promise<Content> {
+  type = typeof type === "string" ? MediaType.fromString(type) : type;
+  const filePath = resolve(templateFile);
+  let lastModified = (await Deno.stat(filePath)).mtime ?? new Date(0);
+  const array: Resource[] = [];
+  for await (const resource of list) {
+    array.push(resource);
+    if (lastModified < resource.lastModified) {
+      lastModified = resource.lastModified;
+    }
+  }
+
+  return new Content(
+    async () => {
+      const tpl = await openTemplate(filePath);
+      return await tpl({
+        list: array,
+        __file__: filePath,
+        __dir__: dirname(filePath),
+        ...metadata,
+        ...context,
+      });
+    },
+    type,
+    language,
+    lastModified,
+    undefined,
+    array.map((r) => r.path.toString()).join("\n"),
+  );
+}
+
+/**
+ * Opens a template file and returns a function that renders the template.
+ * @param templateFile The path to the template file.
+ * @returns A function that renders the template.
+ */
+async function openTemplate(
+  templateFile: string,
+): Promise<(params: Record<string, unknown>) => Promise<string>> {
+  const filePath = resolve(templateFile);
+  const f = await Deno.open(filePath, { read: true });
+  try {
+    return await compile(f);
+  } finally {
+    f.close();
+  }
 }
--- ejs_test.ts
+++ ejs_test.ts
@@ -2,10 +2,12 @@ import { join } from "https://deno.land/std@0.101.0/path/mod.ts";
 import {
   assert,
   assertEquals,
+  assertNotEquals,
 } from "https://deno.land/std@0.101.0/testing/asserts.ts";
 import { Content, LanguageTag, MediaType } from "./content.ts";
-import { renderTemplate } from "./ejs.ts";
-import { withTempDir } from "./fixtures.ts";
+import { renderListTemplate, renderTemplate } from "./ejs.ts";
+import { makeResources, withTempDir } from "./fixtures.ts";
+import { queryTitle, sortResources } from "./metadata.ts";
 
 Deno.test("renderTemplate()", () =>
   withTempDir({
@@ -41,5 +43,65 @@ Deno.test("renderTemplate()", () =>
           { title: "Content title" },
         );
       }
+    },
+  }));
+
+Deno.test("renderListTemplate()", () =>
+  withTempDir({
+    prefix: "jikji-t-",
+    suffix: "-fx",
+    async fn(path: string) {
+      const tpl = join(path, "template.ejs");
+      const tplBody =
+        "<h1><%= heading %></h1><ul><% for (const r of list) { %>" +
+        "<li><%= await queryTitle(r) %>" +
+        "<% } %></ul>";
+      await Deno.writeTextFile(tpl, tplBody);
+      const resources = makeResources({
+        "foo.txt": ["a", { title: "A" }],
+        "bar.txt": ["b", { title: "B" }],
+        "baz.txt": ["c", { title: "C" }],
+        "qux.txt": ["d", { title: "D" }],
+      });
+      const content = await renderListTemplate(
+        tpl,
+        resources,
+        { heading: "Title" },
+        "text/html",
+        "en",
+      );
+      assertEquals(
+        "<h1>Title</h1><ul><li>B<li>C<li>A<li>D</ul>",
+        await content.getBody(),
+      );
+      assertEquals(content.type, MediaType.fromString("text/html"));
+      assertEquals(content.language, LanguageTag.fromString("en"));
+
+      const content2 = await renderListTemplate(
+        tpl,
+        await sortResources(resources, queryTitle),
+        { heading: "Title" },
+        "text/html",
+        "en-US",
+      );
+      assertEquals(
+        "<h1>Title</h1><ul><li>A<li>B<li>C<li>D</ul>",
+        await content2.getBody(),
+      );
+      assertEquals(content2.type, MediaType.fromString("text/html"));
+      assertEquals(content2.language, LanguageTag.fromString("en-US"));
+      assertEquals(content.lastModified, content2.lastModified);
+      assertNotEquals(content.extraFingerprint, content2.extraFingerprint);
+
+      await Deno.writeTextFile(tpl, tplBody);
+      const content3 = await renderListTemplate(
+        tpl,
+        resources,
+        { heading: "Title" },
+        "text/html",
+        "en",
+      );
+      assertEquals(await content3.getBody(), await content.getBody());
+      assert(content3.lastModified > content.lastModified);
     },
   }));
